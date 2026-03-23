3e6ee23679ad310c0561f5faf023a74b13e67953
Ahmad <ahmad.vbe@gmail.com>
2026-03-23 15:05:22 -0400

    implement Better auth

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import express from "express";
 // @ts-ignore
 import cors from "cors";
 
-// import { toNodeHandler } from "better-auth/node";
+import { toNodeHandler } from "better-auth/node";
 
 import subjectsRouter from "./routes/subjects.js";
 import usersRouter from "./routes/users.js";
@@ -44,7 +44,14 @@ app.use(
   })
 );
 
-// app.all("/api/auth/*splat", toNodeHandler(auth));
+// 7-5:45:18 Mount ROUTER Handler:
+//     set up a route handler on ur server : handling the req for the path /api/auth/
+//create a catch all route in case of express js v5 it is api/auth/*splat  for the express js v4 api/auth/*
+ app.all("/api/auth/*splat", //5:46:40 *splat matches any path without the root path
+     toNodeHandler(auth));//pass the isntance of auth 5:47:32 the one we created
+        //so any req matching this path will be handled by BtterAuth for us and it will authenticate users on our behalf 5:49:25
+
+
 
 app.use(express.json());
 
--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -15,18 +15,20 @@ const securityMiddleware = async ( //accepting 3 params
   res: Response,
   next: NextFunction //depending on whether it rates the req as a threat or nt =>it can allow it to pass the next req or nt 5:25:$5
 ) => {
+  console.log("middleware is called ") //this is shown at the BE terminal
 
   // If NODE_ENV is TEST, skip security middleware 5:26:16
   if (process.env.NODE_ENV === "test") { //we dnt hve to do sec analyses on a test env
     return next();
   }
-
+  console.log("process.env.NODE_ENV ",process.env.NODE_ENV)
   try { //5:26:27 define the user role
     const role: RateLimitRole = req.user?.role ?? "guest"; //type.d.ts
     // RateLimitRole creation : 5:27:47 every request will pass through the auth middleware
     // which wlll get the user info and set user role
     // so we can allow certain types of users teacher or admins to make more requests than students 5:28:00
     //lets setup those limits 5:28:08
+    console.log("role of the user is ",role)
     let limit: number;
     let message: string;
 
@@ -53,6 +55,7 @@ const securityMiddleware = async ( //accepting 3 params
         break;
     }
 
+    console.log("limit of the user is ",limit)
     //5:29:34 Create a new ARCJET client
     const client = aj.withRule( //instance of arcjet created in the  webstormproject/classroom-backend/src/config/arcjet.ts
       slidingWindow({ //5:30:05
@@ -61,6 +64,7 @@ const securityMiddleware = async ( //accepting 3 params
         max: limit, //dynamic by setting it to the limit that depends on the user role 5:30:20
       })
     );
+
 
     //5:30:25 Interception of the Request
     const arcjetRequest: ArcjetNodeRequest = {
@@ -74,6 +78,7 @@ const securityMiddleware = async ( //accepting 3 params
 
     //5:31:15 Arcjet Decision making the decision for us based on the params defined for it
     const decision = await client.protect(arcjetRequest);
+    console.log("arcjet decision is ",decision)
 
     //here we can hve difference clauses depending on what the decision of the outcome is
     if (decision.isDenied() && decision.reason.isBot()) { //5:31:50 Bots access exposure
@@ -92,6 +97,7 @@ const securityMiddleware = async ( //accepting 3 params
 
     if (decision.isDenied() && decision.reason.isRateLimit()) { //5:33:00 Rate Limit
       return res.status(429).json({
+             // 5:37:00 other issue HTTP 429 standard for rate limiting instead of 403
         error: "Too Many Requests",
         message, //this message is dynamic depneding on the role under which is the user is authenticate
                   //it is rendered within the switch scenario
